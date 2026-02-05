3656a7950b8cb54c7fa97a645cca8b69248032bc
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-02-05 16:13:39 +0800

    Update 创建环境.sh

--- st/创建环境.sh
+++ st/创建环境.sh
@@ -1,5 +1,12 @@
+# 创建环境st
 conda activate base
 conda create -n st python=3.10.14 -y
 conda activate st
 conda install scanpy -y
 pip install stlearn
+
+# 创建环境cell2location
+conda activate base
+conda create -n cell2location python=3.10.14 -y
+conda activate cell2location
+conda install scanpy -y
