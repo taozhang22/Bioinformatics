9aff05211edd23dcb6b7e5dc95ab125529aeff69
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-02-05 16:33:09 +0800

    Update 创建环境.sh

--- st/创建环境.sh
+++ st/创建环境.sh
@@ -8,8 +8,8 @@ pip install stlearn
 # 创建环境cell2location
 conda activate base
 conda create -n cell2location python=3.10.14 -y
+conda activate cell2location
 pip install ipykernel
 python -m ipykernel install --user --name=cell2location --display-name "cell2location"
-conda activate cell2location
 conda install scanpy -y
 pip install cell2location[tutorials]
