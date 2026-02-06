2909bb583a8dc27e49860ab2b205611966eb3da0
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-02-06 22:01:28 +0800

    Update 整理数据.R

--- singlecell/整理数据.R
+++ singlecell/整理数据.R
@@ -8,12 +8,12 @@ library(qs2)
 setwd("/home/yang/research/bioinformation/singlecell/database")
 
 dir <- "GSE178318"
-counts <- ReadMtx(
+seurat <- ReadMtx(
   mtx = paste0(dir, "/GSE178318_matrix.mtx.gz"),
   features = paste0(dir, "/GSE178318_genes.tsv.gz"),
   cells = paste0(dir, "/GSE178318_barcodes.tsv.gz")
 )
-seurat <- CreateSeuratObject(counts = counts)
+seurat <- CreateSeuratObject(counts = seurat)
 seurat$Sample <- word(rownames(seurat@meta.data), start = 2, end = 3, sep = "_")
 seurat <- split(seurat, seurat$Sample)
 qs_save(seurat, paste0(dir, "/R/",dir, ".qs2"))
