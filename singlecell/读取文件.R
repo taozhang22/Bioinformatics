2082b398054fb1e0588d061472866d815fe07f19
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-02-06 21:58:58 +0800

    Create 读取文件.R

---
+++ singlecell/读取文件.R
@@ -0,0 +1,19 @@
+###################################################################################
+# 方法一：多个10X文件变成三个文件的
+###################################################################################
+rm(list = ls()); gc()
+library(Seurat)
+library(tidyverse)
+library(qs2)
+setwd("/home/yang/research/bioinformation/singlecell/database")
+
+dir <- "GSE178318"
+counts <- ReadMtx(
+  mtx = paste0(dir, "/GSE178318_matrix.mtx.gz"),
+  features = paste0(dir, "/GSE178318_genes.tsv.gz"),
+  cells = paste0(dir, "/GSE178318_barcodes.tsv.gz")
+)
+seurat <- CreateSeuratObject(counts = counts)
+seurat$Sample <- word(rownames(seurat@meta.data), start = 2, end = 3, sep = "_")
+seurat <- split(seurat, seurat$Sample)
+qs_save(seurat, paste0(dir, "/R/",dir, ".qs2"))
