401be880611edd1d6def52e0d2e98dbadbc40415
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-03-10 18:18:25 +0800

    Update 创建环境.sh

--- console/创建环境.sh
+++ console/创建环境.sh
@@ -1,3 +1,82 @@
+# scenv (win版本)
+conda activate base
+conda create -n scenv python=3.11 -y
+conda activate scenv
+pip install ipykernel
+python -m ipykernel install --user --name=scenv --display-name "scenv"
+pip install papermill ipython-autotime
+pip install scanpy igraph leidenalg
+pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
+pip install scvi-tools
+
+
+
+
+
+pip install datatable
+
+conda install rpy2 -y
+
+
+pip install openpyxl
+pip install igraph
+pip install leidenalg
+pip install scikit-image
+pip install harmonypy
+pip install infercnvpy
+pip install gseapy
+pip install sccoda
+pip install liana
+pip install cellphonedb
+pip install palantir
+pip install scvelo
+
+
+
+# scenv (linux版本)
+conda activate base
+conda create -n scenv python=3.10 -y
+conda activate scenv
+conda install r-base=4.4.1 -y
+conda install scanpy -y
+conda install rpy2 -y
+pip install ipykernel
+python -m ipykernel install --user --name=scenv --display-name "scenv"
+pip install papermill
+pip install ipython-autotime
+pip install openpyxl
+pip install igraph
+pip install leidenalg
+pip install scikit-image
+pip install scvi-tools[cuda]
+pip install harmonypy
+pip install infercnvpy
+pip install gseapy
+pip install sccoda
+pip install liana
+pip install cellphonedb
+pip install palantir
+pip install scvelo
+pip install datatable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # R_4.4.1
 conda activate base
 conda create -n R_4.4.1 -y
